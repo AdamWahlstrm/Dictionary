c90d8486f3964850657eaf98a19fa345f0dfe4b4
AdamWahlstrm <wahlstrmadam@gmail.com>
2023-09-21 19:57:46 +0200

    App.jsx

---
+++ dictionary-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dictionary from './Dictionary';
+import './Dictionary';
+
+function App() {
+  return (
+    <div className="App">
+      <Dictionary />
+    </div>
+  );
+}
+
+export default App;
